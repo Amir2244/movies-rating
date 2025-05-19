906eed310a17fcee06a6aee580135f5992181970
Amir Youssef <84175214+Amir2244@users.noreply.github.com>
2025-05-19 20:55:28 +0300

    Add AnalyticsPersistencePort interface for data persistence
    Introduces an output port to define how analytics data is stored. This abstraction allows flexibility for different persistence implementations, such as MongoDB. It includes a method to save analytics data

---
+++ batch-training-service/src/main/java/org/hiast/batch/application/port/out/AnalyticsPersistencePort.java
@@ -0,0 +1,17 @@
+package org.hiast.batch.application.port.out;
+
+import org.hiast.batch.domain.model.DataAnalytics;
+
+/**
+ * Output Port defining how to persist analytics data.
+ * Implementations (adapters) will handle specific persistence stores like MongoDB.
+ */
+public interface AnalyticsPersistencePort {
+    /**
+     * Saves data analytics to the persistence store.
+     *
+     * @param dataAnalytics The analytics data to save.
+     * @return True if the save operation was successful, false otherwise.
+     */
+    boolean saveDataAnalytics(DataAnalytics dataAnalytics);
+}
